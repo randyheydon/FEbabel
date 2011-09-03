754969ace913abad26664b8ccf1019ea4c93310c
Randy Heydon <randy.heydon@clockworklab.net>
2011-09-02 20:22:09 -0400

    A bunch of elements, without much code.  Also some tests.

--- febabel/geometry.py
+++ febabel/geometry.py
@@ -3,9 +3,16 @@ class Node(object):
     The node coordinates can be accessed and set through properties x,y,z.
     Similarly, coordinates can be indexed and iterated over like a list."""
 
+    # Only one piece of data needs storing, so use slots to decrease memory.
+    __slots__ = ['_pos']
+
+
     def __init__(self, pos):
         """Create a node at the given position.
-        pos must be an iterable of length 3."""
+        pos must be an iterable of length 3.
+        NOTE: This will not protect you from yourself!  Insert *only* valid
+        data (length 3 sequence of ints/floats), or the result will be
+        undefined!"""
         self._pos = list(pos)[0:3]
 
 
@@ -36,6 +43,28 @@ class Node(object):
         return self._pos[i]
     def __setitem__(self, i, value):
         self._pos[i] = value
+    def __len__(self):
+        # Could return len(self._pos), but it will always be 3...
+        return 3
 
     def __repr__(self):
         return "%s(%s)" % (self.__class__.__name__, repr(self._pos))
+
+
+
+class Element(object):
+    "Base class for all different element types."
+
+    def __init__(self, nodes, material=None):
+        """nodes is an iterable of Node objects.
+        material is a Material object, or None."""
+        self._nodes = list(nodes)
+        self._material = material
+
+
+class Tet4(Element):
+    "4-node linear tetrahedral element"
+class Hex8(Element):
+    "8-node linear hexahedral (brick) element"
+class Shell4(Element):
+    "4-node shell element"
---
+++ test/test_geometry.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import unittest, os
+
+import sys; sys.path.append('..')
+from febabel import geometry as g
+
+
+class TestGeometry(unittest.TestCase):
+
+
+    def test_init(self):
+        a = g.Node((3,4.5,9))
+
+
+    def test_getters(self):
+        pos = (-1.1, 900.5, 12)
+        a = g.Node(pos)
+        self.assertEqual(a.x, pos[0])
+        self.assertEqual(a.y, pos[1])
+        self.assertEqual(a.z, pos[2])
+        self.assertEqual(a[0], pos[0])
+        self.assertEqual(a[1], pos[1])
+        self.assertEqual(a[2], pos[2])
+        self.assertEqual(list(a), list(pos))
+
+
+    def test_setters(self):
+        pos = (0,0.5,0.05)
+        a = g.Node(pos)
+        a.x = 3
+        self.assertEqual(a.x, 3)
+        a[0] = 12
+        self.assertEqual(a.x, 12)
+        a.y = 32.1
+        a.z = 909
+        self.assertEqual(list(a), [12,32.1,909])
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
