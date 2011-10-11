72f5070653ea4378e78c155eb1a283482c7a6a1b
Randy Heydon <randy.heydon@clockworklab.net>
2011-10-10 22:50:56 -0400

    Added some tests for Elements.

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import unittest, os
 
-import sys; sys.path.append('..')
+import sys; sys.path[0] = os.path.dirname(sys.path[0])
 from febabel import geometry as g
 
 
@@ -34,6 +34,27 @@ class TestNode(unittest.TestCase):
         a.y = 32.1
         a.z = 909
         self.assertEqual(list(a), [12,32.1,909])
+
+
+
+class TestElements(unittest.TestCase):
+
+    def setUp(self):
+        self.nodes = [g.Node(map(float,(i,j,k)))
+            for i in xrange(3) for j in xrange(3) for k in xrange(3)]
+
+
+    def test_init(self):
+        g.Surface3(self.nodes[0:3])
+        g.Tet4(self.nodes[0:4])
+
+
+    def test_getters(self):
+        a = g.Tet4(self.nodes[0:4])
+        for n in xrange(4):
+            self.assertEqual(a[n], self.nodes[n])
+        for i,j in zip(a, self.nodes):
+            self.assertEqual(i, j)
 
 
 
