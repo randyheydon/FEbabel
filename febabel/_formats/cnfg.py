c3bd0f576b976b972d076609d6c634592a370ce6
Randy Heydon <randy.heydon@clockworklab.net>
2011-10-19 14:33:50 -0400

    Implemented node transformation in .cnfg reading.
    But no tests made for it yet.

--- febabel/_formats/cnfg.py
+++ febabel/_formats/cnfg.py
@@ -82,13 +82,49 @@ def read(self, filename):
 
 
     # Collect all nodes in the geometry source files.
+    # FIXME: Go through all sets created by each geo file, not just allnodes?
     nodeset = set()
     for f in geo_files:
         nodeset.update( self.sets[SETSEP.join((f, NSET))] )
     nodeset = list(nodeset)
-    nodearray = np.array(nodeset).T
 
-    # TODO: Transform all nodes involved in this config.
+    # Get transform points from config.
+    trans = dict()
+    for k in ('medial_f_cond', 'lateral_f_cond', 'proximal_femur',
+        'distal_femur', 'proximal_tibia'):
+        trans[k] = np.array(map( float,
+            cp.get('transform', k).split(SEPCHAR) ))
+    q_angle = float(cp.get('transform', 'q_angle'))
+    scale = float(cp.get('options', 'scale'))
+
+    # Determine the three axes of the femoral coordinate system.
+    q_angle_RM = [[1,  0,  0],
+        [0,  np.cos(q_angle), -np.sin(q_angle)],
+        [0,  np.sin(q_angle),  np.cos(q_angle)]]
+    mech_axis = np.dot( q_angle_RM,
+        trans['proximal_femur'] - trans['distal_femur'] )
+    ant_post_axis = np.cross( mech_axis,
+        trans['lateral_f_cond'] - trans['medial_f_cond'] )
+    flex_axis = np.cross( ant_post_axis, mech_axis )
+    # Normalize axes
+    e1_prime = flex_axis / np.linalg.norm( flex_axis )
+    e2_prime = ant_post_axis / np.linalg.norm( ant_post_axis )
+    e3_prime = mech_axis / np.linalg.norm( mech_axis )
+
+    # Rotation and scaling matrix to convert coordinate system.
+    RM = np.array( [e1_prime, e2_prime, e3_prime] ) / scale
+    # Translation vector to bring to new origin.
+    trans_vec = -(trans['distal_femur'] + trans['proximal_tibia']) / 2
+    # Now transform all those points!
+    # All points are offset by the translation vector.  The resulting point
+    # matrix is transposed, so each column represents one point.  This allows
+    # all points to be simultaneously multiplied by the rotation matrix.  The
+    # result is then transposed again to bring it back to one point per row.
+    new_nodeset = np.dot( RM, (np.array(nodeset) + trans_vec).T ).T
+
+    # Set all nodes to their new coordinates.
+    for node, pos in zip(nodeset, new_nodeset):
+        node.x, node.y, node.z = pos
 
 
     # TODO: Something with solver settings.
