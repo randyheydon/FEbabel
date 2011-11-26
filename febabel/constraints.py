1c5b4a1a87f49fc09508d61963e5921952f02106
Randy Heydon <randy.heydon@clockworklab.net>
2011-11-25 19:04:30 -0500

    LoadCurves and Switchables are starting to make some sense.

--- febabel/constraints.py
+++ febabel/constraints.py
@@ -9,17 +9,74 @@ class Constrainable(object):
 
 
 class Switchable(object):
-    "A mixin to allow an object to be activated at specific times."
+    """A mixin to allow an object to be activated at specific times.
+    A switchable object will be active during times that its given loadcurve
+    evaluates to True, and inactive when False."""
+
+    def __init__(self, switchcurve):
+        self.switchcurve = loadcurve
 
 
 
 
 class LoadCurve(object):
-    ""
+    """Defines how a constraint (load, displacement, etc.) varies with time.
 
-# TODO: a few common loadcurves
-loadcurve_zero = LoadCurve()
-loadcurve_instant = LoadCurve()
+    points is a dict with numeric keys and values.  Each key represents a time,
+    while its corresponding value represents the constraint's value.
+
+    interpolation and extrapolation define how the curve behaves between and
+    beyond the given points, respectively.  Valid values for each are contained
+    in this class; all IN_* values can be used for interpolation, EX_* for
+    extrapolation."""
+
+    IN_LINEAR = 'linear'
+    IN_STEP = 'step'
+    IN_CUBIC_SPLINE = 'spline'
+
+    EX_CONSTANT = 'constant'
+    EX_TANGENT = 'tangent'
+    EX_REPEAT = 'repeat'
+    EX_REPEAT_OFFSET = 'repeat offset'
+
+    def __init__(self, points, interpolation=IN_LINEAR, extrapolation=EX_CONSTANT):
+        self.points = points
+        self.interpolation = interpolation
+        self.extrapolation = extrapolation
+
+    # TODO: Call object with a specific time to receive the loadcurve's value
+    # at that time.
+    #def __call__(self, time):
+    #    point_times = self.points.keys()
+    #    point_times.sort()
+
+    #    if point_times[0] <= time <= point_times[-1]:
+    #        if self.interpolation == self.IN_LINEAR:
+    #            pass
+    #        elif self.interpolation == self.IN_STEP:
+    #            pass
+    #        elif self.interpolation == self.IN_CUBIC_SPLINE:
+    #            pass
+    #        else:
+    #            pass
+
+    #    else:
+    #        if self.extrapolation == self.EX_CONSTANT:
+    #            return self.points[ point_times[0 if time <= point_times[0] else -1] ]
+    #        elif self.extrapolation == self.EX_TANGENT:
+    #            pass
+    #        elif self.extrapolation == self.EX_REPEAT:
+    #            return self( time % points_time[-1] )
+    #        elif self.extrapolation == self.EX_REPEAT_OFFSET:
+    #            pass
+    #        else:
+    #            pass
+
+
+# A few common loadcurves.
+loadcurve_zero = LoadCurve({0:0, 1:0})
+loadcurve_constant = LoadCurve({0:1, 1:1})
+loadcurve_ramp = LoadCurve({0:0, 1:1})
 
 
 
@@ -27,8 +84,10 @@ loadcurve_instant = LoadCurve()
 class Constraint(Switchable):
     "Base class for different types of constraints/loads."
 
-    def __init__(self, vector, loadcurve, **kwargs):
-        Switchable.__init__(self)
+    def __init__(self, vector, loadcurve, switchcurve=loadcurve_constant):
+        Switchable.__init__(self, switchcurve)
+        self.vector = vector
+        self.loadcurve = loadcurve
 
 
 class Force(Constraint): pass
@@ -39,3 +98,11 @@ class Fixed(Displacement):
 
 
 
+
+class Contact(Switchable):
+    """Defines a contact interface between two surface sets."""
+
+    def __init__(self, master, slave, switchcurve=loadcurve_constant):
+        Switchable.__init__(self, switchcurve)
+        self.master = master
+        self.slave = slave
